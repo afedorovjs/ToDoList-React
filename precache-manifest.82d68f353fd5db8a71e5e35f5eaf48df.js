a665cc58cda66f319cd8fd16ed3607509b1ae9e3
Aleksandr Fedorov <nsgowest@gmail.com>
2019-09-20 05:05:51 +0300

    Updates

---
+++ precache-manifest.82d68f353fd5db8a71e5e35f5eaf48df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4d26f70a28e032fb8055b76f6287cf",
+    "url": "/ToDoList-React/index.html"
+  },
+  {
+    "revision": "68398bcd134a37db7230",
+    "url": "/ToDoList-React/static/css/main.42823630.chunk.css"
+  },
+  {
+    "revision": "dd8c5f51584bb254f5ee",
+    "url": "/ToDoList-React/static/js/2.b7517bc3.chunk.js"
+  },
+  {
+    "revision": "68398bcd134a37db7230",
+    "url": "/ToDoList-React/static/js/main.20002249.chunk.js"
+  },
+  {
+    "revision": "5a625aa8c0aba80f87c7",
+    "url": "/ToDoList-React/static/js/runtime~main.97193e80.js"
+  },
+  {
+    "revision": "e34f93cc09e99a7a23eab5d78f4b47eb",
+    "url": "/ToDoList-React/static/media/add-button-icon.e34f93cc.svg"
+  },
+  {
+    "revision": "1e71c5ad8188e077f379d7964621a7e2",
+    "url": "/ToDoList-React/static/media/bg.1e71c5ad.jpg"
+  },
+  {
+    "revision": "0b9759562c2bccd4ef5da91dc43c7152",
+    "url": "/ToDoList-React/static/media/checked.0b975956.svg"
+  },
+  {
+    "revision": "a225c98837a170e437cc7ed7767b7e5e",
+    "url": "/ToDoList-React/static/media/circle-outline.a225c988.svg"
+  },
+  {
+    "revision": "650d451f8cf42b4e0610c42357bf31fb",
+    "url": "/ToDoList-React/static/media/refresh.650d451f.svg"
+  },
+  {
+    "revision": "88aeaec4db5e186df8f727ed613b4485",
+    "url": "/ToDoList-React/static/media/trash-hovered.88aeaec4.svg"
+  },
+  {
+    "revision": "47684196dd20226669109204f9c65667",
+    "url": "/ToDoList-React/static/media/trash.47684196.svg"
+  }
+]);
